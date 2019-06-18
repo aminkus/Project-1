bc4e180d024edf1bf38c2bea931357263ff892ab
Joe Hutchinson <48301850+JoeLearns2Code@users.noreply.github.com>
2019-06-17 22:09:25 -0700

    adjustments to js

--- assets/javascript/googlemaps.js
+++ assets/javascript/googlemaps.js
@@ -125,6 +125,8 @@ function searchLocation() {
       });
       foundCity = results[0].address_components[0].long_name;
       foundState = results[0].address_components[2].short_name
+      console.log(foundCity);
+      console.log(foundState);
       
 		} 
 		else 
@@ -133,71 +135,73 @@ function searchLocation() {
 		}
   });
 
-  //Ajax call for keilswolfram app
-
-  var queryURL = "https://keilswolframmess.herokuapp.com/?startCity=" + localCity + "&startState=" + localState + "&endCity=" + foundCity + "&endState=" + foundState
-  
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(queryURL);
-    console.log(response);
-   
-    $("#population").empty();
-    $("#annualMedianHomePrice").empty();
-    $("#unemployment").empty();
-    $("#crime").empty();
-    $("#saleTax").empty();
-
-
-
-    var popImg = $("<img>");
-    popImg.attr('alt', 'population info');
-    popImg.attr('id', 'popData')
-    popImg.attr('src', response.pop);
-    $("#population").append(popImg);
-
-
-    var annualImg = $("<img>");
-    annualImg.attr('alt',"annual median home price");
-    annualImg.attr('id', 'annualMedianHPData');
-    annualImg.attr('src', response.medianH);
-    $("#annualMedianHomePrice").append(annualImg);
-
-    
-    var unemployImg = $("<img>");
-    unemployImg.attr('alt', 'unemployment rate');
-    unemployImg.attr('id', 'unemploymentData');
-    unemployImg.attr('src', response.unemployed);
-    $("#unemployment").append(unemployImg);
-
-
-    var crimeImg = $("<img>");
-    crimeImg.attr('alt','crime rate');
-    crimeImg.attr('id', 'crimeData');
-    crimeImg.attr('src', response.crime);
-    $("#crime").append(crimeImg);
-    
-
-    var salesTaxImg = $("<img>");
-    salesTaxImg.attr('alt', 'sales tax');
-    salesTaxImg.attr('id', 'salesTaxData');
-    salesTaxImg.attr('src', response.sales);
-    $("#saleTax").append(salesTaxImg);
-
-
-
-
-
-  });
-
 
 
 };
 
 
+//Ajax call for keilswolfram app
+
+function searchkeilswolfram() {
+
+var queryURL = "https://keilswolframmess.herokuapp.com/?startCity=" + localCity + "&startState=" + localState + "&endCity=" + foundCity + "&endState=" + foundState
+  
+$.ajax({
+  url: queryURL,
+  method: "GET",
+}).then(function(response) {
+  console.log(queryURL);
+  console.log(response);
+ 
+  $("#population").empty();
+  $("#annualMedianHomePrice").empty();
+  $("#unemployment").empty();
+  $("#crime").empty();
+  $("#saleTax").empty();
 
 
+
+  var popImg = $("<img>");
+  popImg.attr('alt', 'population info');
+  popImg.attr('id', 'popData')
+  popImg.attr('src', response.pop);
+  $("#population").append(popImg);
+
+
+  var annualImg = $("<img>");
+  annualImg.attr('alt',"annual median home price");
+  annualImg.attr('id', 'annualMedianHPData');
+  annualImg.attr('src', response.medianH);
+  $("#annualMedianHomePrice").append(annualImg);
+
+  
+  var unemployImg = $("<img>");
+  unemployImg.attr('alt', 'unemployment rate');
+  unemployImg.attr('id', 'unemploymentData');
+  unemployImg.attr('src', response.unemployed);
+  $("#unemployment").append(unemployImg);
+
+
+  var crimeImg = $("<img>");
+  crimeImg.attr('alt','crime rate');
+  crimeImg.attr('id', 'crimeData');
+  crimeImg.attr('src', response.crime);
+  $("#crime").append(crimeImg);
+  
+
+  var salesTaxImg = $("<img>");
+  salesTaxImg.attr('alt', 'sales tax');
+  salesTaxImg.attr('id', 'salesTaxData');
+  salesTaxImg.attr('src', response.sales);
+  $("#saleTax").append(salesTaxImg);
+
+
+
+
+
+});
+
+
+};
 
 
